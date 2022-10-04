b62c7c006d87536ef657f4a532826548bb279d15
BenIanGifford <BenIanGifford@gmail.com>
2022-10-04 18:48:05 -0400

    Update vending_machine.py
    Added check_for_block function

--- vending_machine.py
+++ vending_machine.py
@@ -14,27 +14,46 @@ pin1 = onionGpio.OnionGpio(1, ignore_busy=True)
 pin1.setDirection(onionGpio.Direction.OUTPUT_LOW)
 print("Outputs set")
 
-
 """ Just defining these things to make the logic easier to understand and help with scalibilty later on"""
 
+def check_for_block():
+    """check if its blocked"""
+    if pin2.getValue.name() == "HIGH":
+        machine_blocked = True
+    else:
+        machine_blocked = False
+        
 def vend_item():
     """A basic vend cycle with a mosfet motor""" 
-    pin1.setValue(onionGpio.Value.HIGH)
-    print("Vending")
-    time.sleep(2)
-    pin1.setValue(onionGpio.Value.LOW)
-    print("Motor stopped")
-    wait_for_light_gate()
+    check_for_block()
+    if mchine_blocked == True:
+        print("Blocked")
+        exit()
+    else:
+        pin1.setValue(onionGpio.Value.HIGH)
+        print("Vending")
+        time.sleep(2)
+        pin1.setValue(onionGpio.Value.LOW)
+        print("Motor stopped")
+        wait_for_light_gate()
 
 def wait_for_light_gate():
     
     """This stuff below is the special part that watches for the light gate to be tripped
-    It waits .25 seconds then if the gate is open, it syas it's not jammed and prints that it's open
+    It waits .25 seconds then if the gate is open, it says it's not jammed and prints that it's open
     If the gate is closed, it says there is an object and waits 5 seconds for you to remove it.
-    If the object is not removed it sets machine blocked to True whic stops the program.
+    If the object is not removed it sets machine blocked to True which stops the program.
     If the object is removed it says vend sucessful and continues dispensing"""
-
-    for i in range(20):
+    
+    time.sleep(1.5)
+    check_for_block()
+    if machine_blocked == False:
+        rasie Exeption("Vend failed")
+    else:
+        print("Item vended")
+        exit()
+        
+    """for i in range(20):
         time.sleep(.25)
 
         if pin2.getValue().name == 'LOW':
@@ -48,10 +67,11 @@ def wait_for_light_gate():
             if pin2.getValue().name == "HIGH": 
                 machine_blocked = True
                 print("Machine blocked")
-                break
+                exit()
             
             else:
                 print("Vend sucessful")
+                machine_blocked == False"""
 
 def wait_for_vend():
     confirmation = input("Vend? \nYes or No? \n>>> ")
@@ -68,4 +88,4 @@ def wait_for_vend():
     this is unideal for production but for now it's fine"""
 
 while machine_blocked == False:
-    wait_for_vend()
+    wait_forvend()
