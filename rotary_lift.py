3f277986049117e2a103c80899a7bf9b159fd7f7
Ben Gifford <beniangifford@gmail.com>
2022-10-07 00:57:00 +0000

    Added full support for vending specific items and scalibilty to as many inputs/outputs as you have pins for

--- rotary_lift.py
+++ rotary_lift.py
@@ -1,12 +1,12 @@
 import onionGpio
 import time
 output_pins = {1:1, 2:3, 3:0}
+
 #this is used to keep track of the items and which pin is used to vend them
 #item number maps to a gpio pin number
-    
-def set_output(out_num=int(input("Item number "))):
-  output_pins[out_num] = onionGpio.OnionGpio(output_pins[out_num], ignore_busy=True)
-  output_pins[out_num].setDirection(onionGpio.Direction.OUTPUT_LOW)
+for key in output_pins:    
+  output_pins[key] = onionGpio.OnionGpio(output_pins[key], ignore_busy=True)
+  output_pins[key].setDirection(onionGpio.Direction.OUTPUT_LOW)
   print("Outputs set")
 
 def vend_item(item_num=int(input("Item number "))):
@@ -14,5 +14,4 @@ def vend_item(item_num=int(input("Item number "))):
     time.sleep(2)
     output_pins[item_num].setValue(onionGpio.Value.LOW)
     
-set_output()
 vend_item()
--- vending_machine.py
+++ vending_machine.py
@@ -1,65 +1,69 @@
 #!/usr/bin/python3
 
-import time
+from time import sleep
 import onionGpio
 machine_blocked = False
 
-"""Set input pins"""
-pin2 = onionGpio.OnionGpio(2, ignore_busy=True)
-pin2.setDirection(onionGpio.Direction.INPUT)
-print("Inputs set")
+output_pins = {1:1, 2:3, 3:0}
 
-"""Set output pins (motors)"""
-pin1 = onionGpio.OnionGpio(1, ignore_busy=True)
-pin1.setDirection(onionGpio.Direction.OUTPUT_LOW)
-print("Outputs set")
+for key in output_pins:
+  output_pins[key] = onionGpio.OnionGpio(output_pins[key], ignore_busy=True)
+  output_pins[key].setDirection(onionGpio.Direction.OUTPUT_LOW)
+  print("Outputs set")
 
-""" Just defining these things to make the logic easier to understand and help with scalibilty later on"""
+input_pins = {1:2, 2:2, 3:2}
+#I only have one light gate but in practice this would take the form:
+#input_pins = {item number:pin for gate}
 
-def check_for_block():
+for key in input_pins:
+    input_pins[key] = onionGpio.OnionGpio(input_pins[key], ignore_busy=True)
+    input_pins[key].setDirection(onionGpio.Direction.INPUT)
+
+
+def check_for_block(port_num):
     """check if its blocked"""
     global machine_blocked
-    if pin2.getValue().name == 'HIGH':
+    if input_pins[port_num].getValue().name == 'HIGH':
         machine_blocked = True
         print(machine_blocked)
     else:
         machine_blocked = False
         
-def vend_item():
+def vend_item(item_num):
     """A basic vend cycle with a mosfet motor""" 
     global machine_blocked
-    check_for_block()
+    check_for_block(item_num)
     if machine_blocked == True:
         print("Blocked")
         exit()
     else:
-        pin1.setValue(onionGpio.Value.HIGH)
+        output_pins[item_num].setValue(onionGpio.Value.HIGH)
         print("Vending")
-        time.sleep(2)
-        pin1.setValue(onionGpio.Value.LOW)
+        sleep(2)
+        output_pins[item_num].setValue(onionGpio.Value.LOW)
         print("Motor stopped")
-        wait_for_light_gate()
+        wait_for_light_gate(item_num)
 
-def wait_for_light_gate():    
+def wait_for_light_gate(port_num):    
     global machine_blocked
-    time.sleep(1.5)
-    check_for_block()
-    if machine_blocked == False:
-        print("Nothing came out")
-        exit()
-        #raise Exception("Vend failed")
-    else:
-        print("Item vended")
-        exit()
-        
-def wait_for_vend():
-    confirmation = input("Vend? \nYes or No? \n>>> ")
+    for i in range(20):
+        sleep(.2)
+        if i == 19: 
+            print("vend failed")
+            exit()
+        elif machine_blocked == False:
+            check_for_block(port_num)
+            print("Checking")
+        elif machine_blocked == True:
+            print("Item vended")
+            exit()
 
-    if  confirmation == "Yes":
-        vend_item()
-    elif confirmation == "No":
-        print("Vend declined")
-    else:
-        print("Bad input. Please say 'Yes' or 'No'")
+def wait_for_vend():
+    while True:
+        item_to_vend = input("Which item would you like to vend?\n 1, 2, 3,\n>>> ")
+        try:
+            vend_item(int(item_to_vend))
+        except ValueError:
+            print("Must be an intiger")
 
 wait_for_vend()
